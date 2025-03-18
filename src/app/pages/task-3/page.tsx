f3ced5361b8552c536cccb2bc4098bf34a6547eb
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2025-03-18 16:03:47 +0400

    Task 3

--- src/app/pages/task-3/page.tsx
+++ src/app/pages/task-3/page.tsx
@@ -1,10 +1,57 @@
+'use client'
 import Header from "@/app/components/Header";
+import { FC, useCallback, useEffect, useState } from "react";
 
 export default function Task3() {
   return <div>
     <Header taskId={3}/>
-    <div>
-      Content of Task 3
+    <div className="flex flex-col gap-2">
+      There are some issues with counter. Try to fix them all.
+      <Counter />
     </div>
   </div>
 }
+
+const Counter: FC = () => {
+  const [count, setCount] = useState<number>(0)
+
+  const increment = useCallback(() => {
+    setCount(count + 1)
+  }, [count])
+
+  useEffect(() => {
+    alert(count)
+  }, [count])
+
+  return <div>
+    <h1><b>Counter:</b> {count}</h1>
+    <button className="button" onClick={increment}>Increment</button>
+  </div>
+}
+
+/*
+
+export default function Task3() {
+  return <div>
+    <Header taskId={3}/>
+    <div className="flex flex-col gap-2">
+      There are some issues with counter. Try to fix them all.
+      <Counter />
+    </div>
+  </div>
+}
+
+const Counter: FC = () => {
+  const [count, setCount] = useState<number>(0)
+
+  const increment = useCallback(() => {
+    setCount(count + 1)
+    alert(count)
+  }, [])
+
+  return <div>
+    <h1><b>Counter:</b> {count}</h1>
+    <button className="button" onClick={increment}>Increment</button>
+  </div>
+}
+*/
