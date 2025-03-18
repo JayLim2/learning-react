45f230f1ea5361c5a8842b574367ddfbf4d11f03
Sergei Komarov <sergei.komarov.qubership@gmail.com>
2025-03-18 15:11:27 +0400

    Task 1

--- src/app/task-1/page.tsx
+++ src/app/task-1/page.tsx
@@ -1,10 +1,53 @@
+'use client'
+import { useEffect, useRef, useState } from "react"
 import Header from "../components/Header"
 
 export default function Task1() {
+  const ref = useRef<HTMLInputElement | null>(null)
+  const [state, setState] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (state) {
+      ref.current?.focus()
+    }
+    setState(true)
+  }, [state])
+
   return <div>
     <Header taskId={1}/>
-    <div>
-      Content of Task 1
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 2,
+    }}>
+      Following input must be focused automatically on page load:
+      <input ref={ref} type='text' placeholder='Some input' style={{ border: '1px solid black', padding: 5, }} />
     </div>
   </div>
 }
+
+/*
+export default function Task1() {
+  const ref = useRef<HTMLInputElement | null>(null)
+  const state = useRef<boolean>(false)
+
+  useEffect(() => {
+    if (state.current) {
+      ref.current?.focus()
+    }
+    state.current = true
+  }, [state.current])
+
+  return <div>
+    <Header taskId={1}/>
+    <div style={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: 2,
+    }}>
+      Following input must be focused automatically on page load:
+      <input ref={ref} type='text' placeholder='Some input' style={{ border: '1px solid black', padding: 5, }} />
+    </div>
+  </div>
+}
+*/
